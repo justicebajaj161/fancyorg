437c03e28bb6a6d3d1a3b0656fa03a92428b8aef
Aditya Gupta <adityabajaj161@gmail.com>
2024-03-27 15:38:34 +0530

    fix third commit

--- schema/user-schema.js
+++ schema/user-schema.js
@@ -1,10 +1,10 @@
 import mongoose from "mongoose";
 
 const UserSchema = new mongoose.Schema({
-    name: { type: String, required: true },
-    email: { type: String, unique: true, required: true },
-    username: { type: String, unique: true, required: true },
-    password: { type: String, required: true },
+    name: { type: String},
+    email: { type: String, unique: true},
+    username: { type: String, unique: true},
+    password: { type: String},
     profilepicture:{ type:String ,default:'0'},
     profilepictureid:{ type:String,default:'0' },
     verified: { type:Boolean, default:false},
